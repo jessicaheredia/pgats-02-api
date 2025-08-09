d45dbba25b18a0947c32bb230e635cc618181c43
Jessica Heredia <jessica.heredia@outlook.com>
2025-08-09 16:49:36 -0300

    Colocando um bug de proposito

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -15,7 +15,7 @@ describe('Transfer Controller', ()=>{
                 value: 200
             });
            expect(resposta.status).to.equal(400);
-           expect(resposta.body).to.have.property('error', "Usuário remetente ou destinatário não encontrado");
+           expect(resposta.body).to.have.property('error', "Usuário remetente ou destinatário encontrado");
         });
     });
     describe('GET /transfer', () =>{
